8d88afa0cd71a802ea1f96c0ac47767e7b578df4
Azamat-V <tester_01av@internet.ru>
2024-04-16 12:53:52 +0300

    First test run

--- base/webdriverfactory.py
+++ base/webdriverfactory.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.options import Options
 
 class WebDriverFactory():
 
@@ -12,13 +14,17 @@ class WebDriverFactory():
             Returns:
             'WebDriver Instance'
         """
+        options = Options()
+        options.add_argument("--headless")
         base_url = "https://www.letskodeit.com/"
         if self.browser == "chrome":
-            driver = webdriver.Chrome()
+            # driver = webdriver.Chrome()
+            driver = webdriver.Chrome(options=options)
         elif self.browser == "firefox":
             driver = webdriver.Firefox()
         else:
-            driver = webdriver.Chrome()
+            # driver = webdriver.Chrome()
+            driver = webdriver.Firefox(options=options)
         driver.implicitly_wait(3)
         driver.maximize_window()
         driver.get(base_url)
